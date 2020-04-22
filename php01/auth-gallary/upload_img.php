596075111b64287e42708b2dcd1cf6e1ce506c00
venustov <venustov@gmail.com>
2020-04-23 00:15:41 +0300

    Кое-что исправил

--- php01/auth-gallary/upload_img.php
+++ php01/auth-gallary/upload_img.php
@@ -2,7 +2,8 @@
 
 // Пути загрузки файлов
 $path = 'img/';
-$tmp_path = 'tmp/';
+// $tmp_path = 'tmp/';
+
 // Массив допустимых значений типа файла
 $types = array('image/gif', 'image/png', 'image/jpeg');
 // Максимальный размер файла
@@ -17,12 +18,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
  // Проверяем размер файла
  if ($_FILES['picture']['size'] > $size) {
    die('Слишком большой размер файла. <a href="?">Попробовать другой файл?</a>');
- } 
+ }
+
+$filePath = $_FILES['picture']['tmp_name'];
+$name = substr(md5_file($filePath),0,10);
+$image = getimagesize($filePath);
+// Сгенерируем расширение файла на основе типа картинки
+$extension = image_type_to_extension($image[2]);
+// Сократим .jpeg до .jpg
+$format = str_replace('jpeg', 'jpg', $extension);
+$name .= $format;
+
  // Загрузка файла и вывод сообщения
- if (!@copy($_FILES['picture']['tmp_name'], $path . $_FILES['picture']['name'])) {
+ if (!@copy($filePath, $path . $name)) {
    echo 'Что-то пошло не так';
  } else {
-   echo 'Загрузка удачна <a href="' . $path . $_FILES['picture']['name'] . '">Посмотреть</a> ';
+   echo 'Загрузка удачна <a href="' . $path . $name . '">Посмотреть</a> ';
  }
 }
+
 ?>
