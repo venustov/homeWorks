8b60c86ce77b7cd59932df317c5dcae2f35263c7
venustov <venustov@gmail.com>
2020-05-08 21:00:25 +0300

    Добавлено отображение ошибок размещения в форме. А также при этом сохранение заполненных полей.

--- php02/siteofnews/views/add.php
+++ php02/siteofnews/views/add.php
@@ -1,14 +1,26 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
     <link rel="stylesheet" type="text/css" href="/styles/current.css">
 </head>
 <body>
+<?php if (!empty($data['error'])): ?>
+    <div class="error">
+      <?php foreach ($data['error'] as $value): ?>
+          <p><?php echo $value; ?></p>
+      <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+<?php if (!empty($data['success'])): ?>
+    <div class="success">
+        <p><?php echo $data['success']; ?></p>
+    </div>
+<?php endif; ?>
 <?php include __DIR__ . '/form.php'; ?>
 </body>
 </html>
--- php02/siteofnews/views/form.php
+++ php02/siteofnews/views/form.php
@@ -1,10 +1,10 @@
 <form class="upload" action="/add.php/addArticle" method="post" enctype="multipart/form-data">
-  <h3>Размещение статьи:</h3>
-  <p><input type="text" id="title" name="title" placeholder="Заголовок статьи"></p>
-  <p><textarea class="text" name="content"></textarea></p>
-  <p><label for="preview">Картинка-превью:</label></p>
-  <p><input type="file" id="preview" name="preview"></p>
-  <p><input type="submit" value="Добавить статью"></p>
-  <!--  <label for="title">Название</label>-->
-  <p><a href="/">На главную</a></p>
+    <h3>Размещение статьи:</h3>
+    <p><input type="text" id="title" name="title" placeholder="Заголовок статьи" required></p>
+    <p><textarea class="text" name="content" required><?php if (!empty($data['content'])): echo $data['content']; endif; ?></textarea></p>
+    <p><label for="preview">Картинка-превью:</label></p>
+    <p><input type="file" id="preview" name="preview" required></p>
+    <p><input type="submit" value="Добавить статью"></p>
+    <!--  <label for="title">Название</label>-->
+    <p><a href="/">На главную</a></p>
 </form>
