c25982b6a8aa85ef99dd8c7e08da9d0bd0e6d380
venustov <venustov@gmail.com>
2020-05-11 20:31:39 +0300

    Все поля в форме добавления - required

--- php02/siteofnews/views/form.php
+++ php02/siteofnews/views/form.php
@@ -1,7 +1,7 @@
 <form class="upload" action="/add.php/addArticle" method="post" enctype="multipart/form-data">
     <h3>Размещение статьи:</h3>
     <p><input type="text" id="title" name="title" placeholder="Заголовок статьи" required></p>
-    <p><textarea class="text" name="content"><?php if (!empty($data['article']->content) && empty($data['success'])): echo $data['article']->content; endif; ?></textarea></p>
+    <p><textarea class="text" name="content" required><?php if ($data['article']->content && empty($data['success'])): echo $data['article']->content; endif; ?></textarea></p>
     <p><label for="preview">Картинка-превью:</label></p>
     <p><input type="file" id="preview" name="preview" required></p>
     <p><input type="submit" value="Добавить статью"></p>
