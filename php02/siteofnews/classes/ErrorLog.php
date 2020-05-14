f5d9fbb31ce34c2069888d27958d3c5b718bdc2e
venustov <venustov@gmail.com>
2020-05-15 01:02:44 +0300

    просмотр записей errorLog на странице

--- php02/siteofnews/classes/ErrorLog.php
+++ php02/siteofnews/classes/ErrorLog.php
@@ -6,7 +6,7 @@ class ErrorLog
 
   public function __construct($record)
   {
-    @file_put_contents(__DIR__ . '/../error/log.txt', $record . PHP_EOL, FILE_APPEND|LOCK_EX);
+    @file_put_contents(__DIR__ . '/../error/log.txt', $record . '<br>', FILE_APPEND|LOCK_EX);
   }
 
 }
